f8510602d534f7c75a8394dfcadf49be3df415a0
Sergei Golos <sergei.golos@gmail.com>
2014-08-03 11:46:06 -0400

    Angular bootstrapping
    Updated the application bundles and views to bootstrap angular and start
    an angular application.

---
+++ PaleoChallenge.UI/App/src/app.js
@@ -0,0 +1,11 @@
+﻿(function() {
+    var app = angular.module('paleo', ['rx']);
+
+    app.controller('test', [
+        '$scope',
+        function($scope) {
+            $scope.text = 'test';
+        }
+    ]);
+
+})();
